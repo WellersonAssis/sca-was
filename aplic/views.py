61c6d4c9f369aab127ab4ee37d1303e70bd58560
Wellerson Assis <wellerson18assis@outlook.com>
2022-11-01 20:31:32 -0300

    vai

--- aplic/views.py
+++ aplic/views.py
@@ -2,4 +2,9 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html' 
+    template_name = 'index.html'
+
+
+class SobreView(TemplateView):
+
+    template_name = 'about-us.html'
